cc6b2ab45247b8481f03af683fdf0e50cdfffde3
Iván Muñoz López <imunozlo@uoc.edu>
2026-05-25 18:35:04 +0200

    no message

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  entorno: 'Produccion',
+  useHash: true,
+  oauthSecretPass:
+    'ZnJhc2VzbGFyZ2FzcGFyYWNvbG9jYXJjb21vY2xhdmVlbnVucHJvamVjdG9kZWVtZXBsb3BhcmFqd3Rjb25zcHJpbmdzZWN1cml0eQ==bWlwcnVlYmFkZWVqbXBsb3BhcmFiYXNlNjQ=',
+  apiUrl: '/apiSetalia',
+};
